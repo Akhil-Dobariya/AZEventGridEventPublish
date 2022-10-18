10e7a514b7e1a5ab67df4c734fc1dc55e01fa391
Akhil-Dobariya <akhil.dobariya@outlook.com>
2022-10-18 19:12:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+using System;
+using System.Threading.Tasks;
+
+namespace AZEventGridEventPublish
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            EventGridPublisherClient publisherClient = new EventGridPublisherClient(
+                new Uri("https://learningegapd.eastus2-1.eventgrid.azure.net/api/events"),
+                new AzureKeyCredential("kPrAIXrnAhuAIydQvPvkWL1a22B+F1ooj1VWHE7C70g="));
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"Event sending {i}");
+
+                EventGridEvent eventGridEvent = new EventGridEvent("LearningEventSubject",
+                "Learning.EventGrid.Test",
+                "1.0",
+                $"[Test Event Data {i}]");
+
+                await publisherClient.SendEventAsync(eventGridEvent);
+
+                Console.WriteLine($"Event sent {i}");
+            }
+
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadLine();
+        }
+    }
+}
